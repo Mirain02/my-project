b93825f96feb99ecc4044774fd69f8a9a86f381c
Mirain02 <inzenerk@gmail.com>
2026-05-06 09:23:56 +0500

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import VAchievements from "@/components/VAchievements.vue";
 import VCustomer from "@/components/VCustomer.vue";
 import VCta from "@/components/VCta.vue";
 </script>
+
 <template>
   <main>
     <VPromo />
